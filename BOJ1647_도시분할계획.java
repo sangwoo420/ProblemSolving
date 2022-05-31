1f6cfb8f637c91840a659e4221cb4c1bef404a44
sangwoo <sangwoo420@gmail.com>
2022-05-31 14:52:35 +0900

    220531

---
+++ BOJ1647_도시분할계획.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ1647_도시분할계획 {
+    private static class Node {
+        int v, w;
+        Node link;
+        Node(int v, int w, Node link) {
+            this.v = v;
+            this.w = w;
+            this.link = link;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int V = Integer.parseInt(st.nextToken()), E = Integer.parseInt(st.nextToken());
+        Node[] adjList = new Node[V + 1];
+        for(int i = 0; i < E; i++) {
+            st = new StringTokenizer(input.readLine());
+            int from = Integer.parseInt(st.nextToken()), to = Integer.parseInt(st.nextToken()), w = Integer.parseInt(st.nextToken());
+            adjList[from] = new Node(to, w, adjList[from]);
+            adjList[to] = new Node(from, w, adjList[to]);
+        }
+        int answer = 0;
+        boolean[] visited = new boolean[V + 1];
+        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return Integer.compare(o1.w, o2.w);
+            }
+        });
+        pq.offer(new Node(1, 0, null));
+        int max = Integer.MIN_VALUE;
+        while(!pq.isEmpty()) {
+            Node tmp = pq.poll();
+            if(visited[tmp.v]) continue;
+            visited[tmp.v] = true;
+            answer += tmp.w;
+            if(max < tmp.w) max = tmp.w;
+            for(Node next = adjList[tmp.v]; next != null; next = next.link) {
+                if(!visited[next.v]) {
+                    pq.offer(next);
+                }
+            }
+        }
+        answer -= max;
+        System.out.println(answer);
+    }
+}

87f367c2fa56a42d1b0a66f2b87569a2c38ca922
sangwoo <sangwoo420@gmail.com>
2022-07-29 17:07:25 +0900

    220729

---
+++ BOJ10926_QuestionMark.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class BOJ10926_QuestionMark {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).next() + "??!");
+    }
+}
